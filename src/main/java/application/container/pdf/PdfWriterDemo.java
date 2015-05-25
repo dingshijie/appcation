850d55214d67b1f45e612179cf23f2a7a1f0bd47
Dingshijie <1129277540@qq.com>
2015-05-25 16:24:24 +0800

    修改pdf生成，设置页眉和页脚

--- src/main/java/application/container/pdf/PdfWriterDemo.java
+++ src/main/java/application/container/pdf/PdfWriterDemo.java
@@ -17,15 +17,18 @@ import com.itextpdf.text.Font;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.Section;
 import com.itextpdf.text.pdf.BaseFont;
-import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfPageEventHelper;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class PdfWriterDemo {
 
 	private static Logger logger = LoggerFactory.getLogger(PdfWriterDemo.class);
+
+	private static BaseFont baseFont = null;//设置基本的文字模式
 
 	public static void main(String[] args) throws IOException {
 
@@ -36,7 +39,6 @@ public class PdfWriterDemo {
 		file.createNewFile();
 		PdfWriterDemo demo = new PdfWriterDemo();
 		Document document = null;
-		BaseFont baseFont = null;
 		try {
 			//这里由于无法显示中文文字到pdf,设置编码的iTextAsian有在maven仓库中不存在，这里就暂时使用这个方式，回头打包成资源包进来也可以
 			baseFont = BaseFont.createFont("c:\\windows\\fonts\\simsun.ttc,1", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
@@ -234,7 +236,7 @@ public class PdfWriterDemo {
 		Chapter chapter_1 = demo.createChapter(document, "体系结构", chapter_font, 2, 5);
 
 		//section 1
-		Section section_1 = demo.createSection(chapter_1, "第1部分 hibernate示例", section_font);
+		Section section_1 = demo.createSection(chapter_1, "概况（Overview）", section_font);
 
 		StringBuffer content1_1 = new StringBuffer("由于 Hibernate 非常灵活，且支持多种应用方案， 所以我们这只描述一下两种极端的情况：“最小”和“全面解决”的体系结构方案。");
 		content1_1.append("“最小”的体系结构方案，要求应用程序提供自己的 JDBC 连接并管理自己的事务。这种方案使用了Hibernate API 的最小子集。");
@@ -250,13 +252,32 @@ public class PdfWriterDemo {
 		StringBuffer content2_1 = new StringBuffer(
 				"一个持久化类的实例可能处于三种不同状态中的某一种。这三种状态的定义则与所谓的持久化上下文（persistence context） 有关。");
 		content2_1.append(" Hibernate 的 Session 对象就是这个所谓的持久化上下文。这三种不同的状态如下：");
-		content2_1.append("瞬态（transient）该实例从未与任何持久化上下文关联过。它没有持久化标识（相当于主键值）。");
-		content2_1.append("持久化（persistent）实例目前与某个持久化上下文有关联。它拥有持久化标识（相当于主键值），并且可能在数据库中有一个对应的行。");
-		content2_1.append("脱管（detached）实例曾经与某个持久化上下文发生过关联，不过那个上下文被关闭了，或者这个实例是被序列化（serialize）到另外的进程。");
+		StringBuffer content2_2 = new StringBuffer("瞬态（transient）");
+		StringBuffer content2_3 = new StringBuffer("该实例从未与任何持久化上下文关联过。它没有持久化标识（相当于主键值）。");
+		StringBuffer content2_4 = new StringBuffer("持久化（persistent）");
+		StringBuffer content2_5 = new StringBuffer("实例目前与某个持久化上下文有关联。它拥有持久化标识（相当于主键值），并且可能在数据库中有一个对应的行。");
+		StringBuffer content2_6 = new StringBuffer("脱管（detached）");
+		StringBuffer content2_7 = new StringBuffer("实例曾经与某个持久化上下文发生过关联，不过那个上下文被关闭了，或者这个实例是被序列化（serialize）到另外的进程。");
 
 		Paragraph paragraph2_1 = demo.createParagraph(20f, 24f, content2_1.toString(), paragraph_font);
 
+		Paragraph paragraph2_2 = demo.createParagraph(20f, 0f, content2_2.toString(), paragraph_font);
+		Paragraph paragraph2_3 = demo.createParagraph(20f, 24f, content2_3.toString(), paragraph_font);
+		Paragraph paragraph2_4 = demo.createParagraph(20f, 0f, content2_4.toString(), paragraph_font);
+		Paragraph paragraph2_5 = demo.createParagraph(20f, 24f, content2_5.toString(), paragraph_font);
+		Paragraph paragraph2_6 = demo.createParagraph(20f, 0f, content2_6.toString(), paragraph_font);
+		Paragraph paragraph2_7 = demo.createParagraph(20f, 24f, content2_7.toString(), paragraph_font);
+
 		section_2.add(paragraph2_1);
+
+		section_2.add(new Paragraph("\n"));//添加一个空行
+
+		section_2.add(paragraph2_2);
+		section_2.add(paragraph2_3);
+		section_2.add(paragraph2_4);
+		section_2.add(paragraph2_5);
+		section_2.add(paragraph2_6);
+		section_2.add(paragraph2_7);
 
 		try {
 			document.add(chapter_1);
@@ -276,7 +297,7 @@ public class PdfWriterDemo {
 	 */
 	public Document createDocument(File file) {
 		try {
-			Document document = new Document(PageSize.A4, 50, 50, 50, 50);
+			Document document = new Document(PageSize.A4, 90, 90, 70, 50);
 			PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file, false));
 
 			Header header = new Header();
@@ -363,7 +384,7 @@ public class PdfWriterDemo {
 
 	/**
 	 * 内部类
-	 * 设置页脚
+	 * 设置页眉和页脚
 	 * @author dingshijie
 	 *
 	 */
@@ -371,10 +392,75 @@ public class PdfWriterDemo {
 
 		@Override
 		public void onEndPage(PdfWriter writer, Document document) {
-
-			ColumnText.showTextAligned(writer.getDirectContent(), Element.ALIGN_CENTER,
-					new Phrase(String.format("%d", writer.getPageNumber())), 300, 50, 0);
+			//设置页眉
+			setHeader(writer, document);
+			//页脚设置
+			setFooter(writer, document);
 		}
 
+		/**
+		 * 设置页眉
+		 * @param writer
+		 * @param document
+		 */
+		private void setHeader(PdfWriter writer, Document document) {
+			//这里我推算的结果为：
+			//left ,leftMargin ,right ,rightMargin是以"起笔点"算起，到对应左右"页面边缘"的位置
+			//top  ,topMargin  ,buttom,buttomMargin是以"起笔点"算起,到对应"页面边缘"的位置 ,其中top为页面高度，应理解为到底侧边缘的距离
+			//理解了上面，下面的引用就可以理解了
+			Font font = new Font(baseFont, 10f, Font.NORMAL, new BaseColor(0, 0, 0));
+
+			//设置页眉方法一(有人说该方式只适合A4纸，具体可以根据情况选择)：
+			//			float x_top = (document.right() + document.leftMargin()) / 2.0f;
+			//			float y_top = (document.top() + 20f);
+			//			ColumnText.showTextAligned(writer.getDirectContent(), Element.ALIGN_CENTER,
+			//					new Phrase(String.format("%s", "hibernate中文文档"), font), x_top, y_top, 0);
+
+			//设置页眉方法二：
+			PdfPTable table = new PdfPTable(3);//这里设置了三列，对于没有用到的列我设置的内容为空，在下面可以见到
+			//如果页眉显示多个信息可用 table.setWidths 进行拆分设置列宽
+			table.setTotalWidth(document.right() - document.rightMargin());//设置总宽度
+			table.setLockedWidth(true);//锁定列宽
+
+			table.getDefaultCell().setFixedHeight(20f);//固定高度
+			table.getDefaultCell().setBorder(Rectangle.BOTTOM);//设置只显示底部边框（这样边框就变成了页眉的分割线）
+			table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);//设置单元格对其方式，右对齐
+			table.getDefaultCell().setBorderColor(new BaseColor(0, 0, 0));//设置边框的颜色
+			table.getDefaultCell().setBorderWidth(0.5f);//设置边框的宽度
+			//添加表头
+			table.addCell("");
+			table.addCell(new Phrase(String.format("%s", "hibernate中文文档"), font));
+			table.addCell("");
+			//设置边框所在的位置
+			table.writeSelectedRows(0, -1, document.leftMargin(), document.top() + 40f, writer.getDirectContent());
+		}
+
+		/**
+		 * 设置页脚
+		 * 这里之所以和header分开，因为一般设置的页眉和页脚都是不同的样式
+		 * @param writer
+		 * @param document
+		 */
+		private void setFooter(PdfWriter writer, Document document) {
+
+			Font font = new Font(baseFont, 10f, Font.NORMAL, new BaseColor(0, 0, 0));
+
+			//设置页眉方法二：
+			PdfPTable table = new PdfPTable(1);//这里设置了三列，对于没有用到的列我设置的内容为空，在下面可以见到
+			//如果页眉显示多个信息可用 table.setWidths 进行拆分设置列宽
+			table.setTotalWidth(document.right() - document.rightMargin());//设置总宽度
+			table.setLockedWidth(true);//锁定列宽
+
+			table.getDefaultCell().setFixedHeight(20f);//固定高度
+			table.getDefaultCell().setBorder(Rectangle.TOP);//设置只显示底部边框（这样边框就变成了页眉的分割线）
+			table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);//设置单元格对其方式，右对齐
+			table.getDefaultCell().setBorderColor(new BaseColor(0, 0, 0));//设置边框的颜色
+			table.getDefaultCell().setBorderWidth(0.5f);//设置边框的宽度
+			//添加表头
+			table.addCell(new Phrase(String.format("%d", writer.getPageNumber()), font));
+			//设置边框所在的位置
+			table.writeSelectedRows(0, -1, document.leftMargin(), document.bottomMargin() + 20f,
+					writer.getDirectContent());
+		}
 	}
 }
